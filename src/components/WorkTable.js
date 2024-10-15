f707be28c0d4cad202ca4e1f0fb22b162a7be3aa
dyzioq <dycha2217@gmail.com>
2024-10-15 18:08:35 +0200

    updated work experience

--- src/components/WorkTable.js
+++ src/components/WorkTable.js
@@ -1,49 +1,56 @@
-import Table from "react-bootstrap/Table";
-
 export default function WorkTable() {
   return (
     <table className="table-borderless experience-table">
       <tbody>
-        <tr>
-          <th colSpan={5}>My Experience</th>
-        </tr>
-        <tr></tr>
-        <tr>
-          <th>Position</th>
-          <th></th>
-          <th></th>
-          <th></th>
-          <th>Company</th>
-        </tr>
-        <tr>
-          <td>Warehouse Worker</td>
-          <td>November 2021</td>
-          <td> - </td>
-          <td>May 2023</td>
-          <td>Decathlon</td>
-        </tr>
+      <tr>
+        <th colSpan={5}>My Experience</th>
+      </tr>
+      <tr></tr>
+      <tr>
+        <th>Position</th>
+        <th></th>
+        <th></th>
+        <th></th>
+        <th>Company</th>
+      </tr>
 
-        <tr>
-          <td>Steward at the match</td>
-          <td>October 2020</td>
-          <td> - </td>
-          <td>August 2021</td>
-          <td>Jagiellonia Białystok</td>
-        </tr>
-        <tr>
-          <td>Call Center</td>
-          <td>Jule 2021</td>
-          <td> - </td>
-          <td>August 2021</td>
-          <td>Delta Contact</td>
-        </tr>
-        <tr>
-          <td>Shop Assistant</td>
-          <td>June 2020</td>
-          <td> - </td>
-          <td>August 2020</td>
-          <td>Piekarnia Okruszek</td>
-        </tr>
+      <tr>
+        <td>Intern Web Developer</td>
+        <td>July 2024</td>
+        <td> -</td>
+        <td>September 2024</td>
+        <td>Multiplay</td>
+      </tr>
+
+      <tr>
+        <td>Warehouse Worker</td>
+        <td>November 2021</td>
+        <td> -</td>
+        <td>May 2023</td>
+        <td>Decathlon</td>
+      </tr>
+
+      <tr>
+        <td>Steward at the match</td>
+        <td>October 2020</td>
+        <td> -</td>
+        <td>August 2021</td>
+        <td>Jagiellonia Białystok</td>
+      </tr>
+      <tr>
+        <td>Call Center</td>
+        <td>Jule 2021</td>
+        <td> -</td>
+        <td>August 2021</td>
+        <td>Delta Contact</td>
+      </tr>
+      <tr>
+        <td>Shop Assistant</td>
+        <td>June 2020</td>
+        <td> -</td>
+        <td>August 2020</td>
+        <td>Piekarnia Okruszek</td>
+      </tr>
       </tbody>
     </table>
   );
